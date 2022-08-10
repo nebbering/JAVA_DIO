0ce91ab5db3cb2560a1d421c1da1dbfdd11225fb
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2022-08-10 16:37:05 -0300

    Up

--- JAVA_DIO/src/poo_model/Endereco.java
+++ JAVA_DIO/src/poo_model/Endereco.java
@@ -2,4 +2,20 @@ package poo_model;
 
 public class Endereco {
 
+	public enum TipoEndereco {
+		RESIDENCIAL,
+		ENTREGA,
+		TRABALHO
+	}
+	
+	public Integer codigo;
+	public String nome;
+	public String cpf;
+	
+	public String endereco;
+	public String numero;
+	public String complemento;
+	public String cidade;
+	public String bairro;
+	
 }
